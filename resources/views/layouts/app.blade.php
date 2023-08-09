0e644092a08a733bc784407fa666058dd8f840bd
Custódio Junior <crbj2506@gmail.com>
2023-08-09 08:12:16 -0300

    Configuração Livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<html>
+
+    <head>
+        <title>Livewire</title>`
+
+        @livewireStyles
+    </head>
+
+    <body>
+
+        <div class="container">
+            {{ $slot }}
+        </div>
+
+
+        @livewireScripts
+    </body>
+
+</html>
